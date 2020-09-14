ff5ebda9979821e33f86f67ffffc02e255e8d813
Az <thomas.guyamier+Kraken@gmail.com>
2020-09-14 14:00:33 +0200

    Ajout Flock et Flock agent behavior

---
+++ TPAI02/Assets/BehaviorScripts/AlignementBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Alignment")]
+public class AlignementBehavior : FlockBehavior
+{
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        //if no neighbor maintain current alignment
+        if (context.Count == 0)
+        {
+            return agent.transform.forward;
+        }
+
+        // add all points toogether and average
+        Vector3 alignmentMove = Vector3.zero;
+        foreach (Transform item in context)
+        {
+            alignmentMove += item.transform.forward;
+        }
+        alignmentMove /= context.Count;
+
+        return alignmentMove;
+
+    }
+}
